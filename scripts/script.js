74a0bdc79ed7e5c86f4a5f6f2eca8376f9490228
Jamie Vos <Jamievos100@gmail.com>
2025-09-04 13:41:33 +0200

    :has, and new js functie ✨

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,6 @@
-let button = document.querySelector('input[type="checkbox"');
-let info = document.querySelector('section:nth-of-type(3)')
+let button = document.querySelector('.alert');
 
-button.addEventListener('change', function(){
-  if(button.checked) {
-    document.body.style.background = "#212121";
-    info.style.background = "grey"
-    console.log("Dark mode!");
+button.addEventListener('mouseover', function(){
+    alert("Mediacollege Amsterdam")
   }
-  else {
-    document.body.style.background = "#CCCCFF"
-     info.style.background = "white"
-    console.log("Light mode!");
-  }
-})
+)
