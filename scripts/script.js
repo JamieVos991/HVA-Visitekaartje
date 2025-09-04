1c8a8bf810c6584f801711ec0ebd97cce753a320
Jamie Vos <Jamievos100@gmail.com>
2025-09-04 11:40:15 +0200

    fixes op mobiel 🐛

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 let button = document.querySelector('input[type="checkbox"');
+let info = document.querySelector('section:nth-of-type(3)')
 
 button.addEventListener('change', function(){
   if(button.checked) {
     document.body.style.background = "#212121";
+    info.style.background = "grey"
     console.log("Dark mode!");
   }
   else {
     document.body.style.background = "#CCCCFF"
+     info.style.background = "white"
     console.log("Light mode!");
   }
 })
